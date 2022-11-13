96e06c26c2ec9f4146d3a9520e28bdf7615f7840
unknown <elizeucosta21@gmail.com>
2022-11-13 05:43:14 -0300

    feat: configuracao cors

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import "reflect-metadata";
+import cors from "cors";
 import express from "express";
 import "express-async-errors";
 import checkErrorMiddleware from "./middlewares/checkError.middleware";
@@ -7,6 +8,7 @@ import loginRoutes from "./routes/login.routes";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use("/users", userRoutes);
 app.use("/login", loginRoutes);
