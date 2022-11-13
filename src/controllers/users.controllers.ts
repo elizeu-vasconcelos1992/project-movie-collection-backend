c3083a0842c0d2a30dcc2826f0c6e742e76a338c
unknown <elizeucosta21@gmail.com>
2022-11-13 16:55:47 -0300

    feat: criacao da listagem de usuario

--- src/controllers/users.controllers.ts
+++ src/controllers/users.controllers.ts
@@ -3,11 +3,18 @@ import { instanceToPlain } from "class-transformer";
 import { IUserLogin, IUserRequest } from "../interfaces/users";
 import createUserService from "../services/users/createUser.service";
 import loginService from "../services/login/createLogin.service";
+import listUserServices from "../services/users/listUser.service";
 
 async function createUserController(req: Request, res: Response) {
   const user: IUserRequest = req.body;
   const newUser = await createUserService(user);
   return res.status(201).json({ data: instanceToPlain(newUser) });
+}
+
+async function listUserController(req: Request, res: Response) {
+  const userId = req.params.id;
+  const user = await listUserServices(userId);
+  return res.status(200).json({ data: instanceToPlain(user) });
 }
 
 async function loginController(req: Request, res: Response) {
@@ -16,4 +23,4 @@ async function loginController(req: Request, res: Response) {
   return res.status(200).json({ data: response });
 }
 
-export { createUserController, loginController };
+export { createUserController, listUserController, loginController };
